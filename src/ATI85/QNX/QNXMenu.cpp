38bd0fec734f35454a29c33295275bedfa8e7d03
tredpath <travis_redpath@live.ca>
2012-10-23 01:32:10 -0500

    Added msg_box2 for getting user responses, refreshing of ROM directory during loading, and disabled ROM changing when using BB10 as context menus appear broken currently in BB10.

--- src/ATI85/QNX/QNXMenu.cpp
+++ src/ATI85/QNX/QNXMenu.cpp
@@ -5,11 +5,14 @@
  *      Author: Travis Redpath
  */
 
+#include <bps/deviceinfo.h>
+
 #include "QNXMenu.h"
 #include "../TI85.h"
 #include "../../EMULib/QNX/LibQNX.h"
 
 #include <stdlib.h>
+#include <strings.h>
 
 extern const char *Title;	/* Program title */
 
@@ -40,16 +43,66 @@ std::vector<std::string> sortAlpha(std::vector<std::string> sortThis)
 	return sortThis;
 }
 
-void UpdateRomList()
+int UpdateRomListError()
+{
+	int retval = 1;
+	dialog_instance_t alert_dialog = 0;
+	dialog_request_events(0);
+	if (dialog_create_alert(&alert_dialog) != BPS_SUCCESS)
+		fprintf(stderr, "Failed to create alert dialog.");
+	dialog_set_title_text(alert_dialog, "Almost-TI Error Report");
+	if (dialog_set_alert_html_message_text(alert_dialog, "<u><b>ERROR:</b> You do not have any ROMS!<br></u><b>Add TI ROMS to:</b> <br>\"<u>shared/misc/TI/ROM</u>\"<br>using either <i>WiFi sharing</i> or <i>Desktop Software.</i>") != BPS_SUCCESS)
+	{
+		fprintf(stderr, "Failed to set alert dialog message text.");
+		dialog_destroy(alert_dialog);
+		alert_dialog = 0;
+	}
+
+	char* cancel_button_context = (char*)"Cancelled";
+	char* ok_button_context = (char*)"OK";
+
+	dialog_add_button(alert_dialog, "Close", true, cancel_button_context, true);
+	dialog_add_button(alert_dialog, "Refresh", true, ok_button_context, true);
+
+	if (dialog_show(alert_dialog) != BPS_SUCCESS)
+	{
+		fprintf(stderr, "Failed to show alert dialog.");
+		dialog_destroy(alert_dialog);
+		alert_dialog = 0;
+	}
+
+	while(1)
+	{
+		bps_event_t* event = NULL;
+		bps_get_event(&event, -1);
+
+		if (event)
+		{
+			if (bps_event_get_domain(event) == dialog_get_domain())
+			{
+				const char* context = dialog_event_get_selected_context(event);
+				if (strcmp(context, cancel_button_context) == 0)
+					retval = 0;
+				break;
+			}
+		}
+	}
+	if (alert_dialog)
+		dialog_destroy(alert_dialog);
+	return retval;
+}
+
+int UpdateRomList(int allowRetry)
 {
 	DIR* dirp;
 	struct dirent* direntp;
 
+SCANDIR:
 	vsList.clear();
 	sortedvecList.clear();
 	//vsList.push_back(Title);
 
-	dirp = opendir("shared/misc/TI/ROM/");
+	dirp = opendir("/accounts/1000/shared/misc/TI/ROM/");
 	if (dirp != NULL)
 	{
 		for (;;)
@@ -65,7 +118,7 @@ void UpdateRomList()
 				continue;
 			int i;
 			for (i = 0; Config[i].ROMFile; i++)
-				if (strcmp(tmp.c_str(), Config[i].ROMFile) == 0)
+				if (strcasecmp(tmp.c_str(), Config[i].ROMFile) == 0)
 				//if (tmp.compare(Config[i].ROMFile) == 0)
 					break;
 			if (Config[i].ROMFile)
@@ -75,58 +128,17 @@ void UpdateRomList()
 	else
 		fprintf(stderr, "dirp is NULL ...\n");
 	fprintf(stderr, "number of files %d\n", vsList.size());
+	closedir(dirp);
 
-	if (vsList.size() == 1)
+	if (vsList.size() == 0)
 	{
-		dialog_instance_t alert_dialog = 0;
-		dialog_request_events(0);
-		if (dialog_create_alert(&alert_dialog) != BPS_SUCCESS)
-			fprintf(stderr, "Failed to create alert dialog.");
-		dialog_set_title_text(alert_dialog, "Almost-TI Error Report");
-		if (dialog_set_alert_html_message_text(alert_dialog, "<u><b>ERROR:</b> You do not have any ROMS!<br></u><b>Add TI ROMS to:</b> <br><u>\"shared/misc/TI/ROM\"</u><br>using either <i>WiFi sharing</i> or <i>Desktop Software.</i>") != BPS_SUCCESS)
-		{
-			fprintf(stderr, "Failed to set alert dialog message text.");
-			dialog_destroy(alert_dialog);
-			alert_dialog = 0;
-		}
-
-		char* cancel_button_context = (char*)"Cancelled";
-
-		if (dialog_add_button(alert_dialog, "OK", true, cancel_button_context, true) != BPS_SUCCESS)
-		{
-			fprintf(stderr, "Failed to add button to alert dialog.");
-			dialog_destroy(alert_dialog);
-			alert_dialog = 0;
-		}
-
-		if (dialog_show(alert_dialog) != BPS_SUCCESS)
-		{
-			fprintf(stderr, "Failed to show alert dialog.");
-			dialog_destroy(alert_dialog);
-			alert_dialog = 0;
-		}
-
-		while(1)
-		{
-			bps_event_t* event = NULL;
-			bps_get_event(&event, -1);
-
-			if (event)
-			{
-				if (bps_event_get_domain(event) == dialog_get_domain())
-				{
-					int selectedIndex = dialog_event_get_selected_index(event);
-					const char* label = dialog_event_get_selected_label(event);
-					const char* context = dialog_event_get_selected_context(event);
-					exit(-1);
-				}
-			}
-		}
-		if (alert_dialog)
-			dialog_destroy(alert_dialog);
+		if (allowRetry && UpdateRomListError())
+			goto SCANDIR;
+		return 0;
 	}
 
 	sortedvecList = sortAlpha(vsList);
+	return 1;
 }
 
 int msg_box1(const char *title, const char *message)
@@ -174,6 +186,69 @@ int msg_box1(const char *title, const char *message)
 		dialog_destroy(alert_dialog);
 
 	return 0;
+}
+
+int msg_box2(const char *title, const char *message)
+{
+	int retval = 0;
+	dialog_instance_t alert_dialog = 0;
+	dialog_request_events(0);
+	if (dialog_create_alert(&alert_dialog) != BPS_SUCCESS)
+		fprintf(stderr, "Failed to create alert dialog.");
+	dialog_set_title_text(alert_dialog, title);
+	if (dialog_set_alert_html_message_text(alert_dialog, message) != BPS_SUCCESS)
+	{
+		fprintf(stderr, "Failed to set alert dialog message text.");
+		dialog_destroy(alert_dialog);
+		alert_dialog = 0;
+	}
+
+	char* cancel_button_context = (char*)"Cancelled";
+	char* ok_button_context = (char*)"Okay";
+
+	if (dialog_add_button(alert_dialog, "Try Anyway", true, ok_button_context, true) != BPS_SUCCESS)
+	{
+		fprintf(stderr, "Failed to add button to alert dialog.");
+		dialog_destroy(alert_dialog);
+		alert_dialog = 0;
+	}
+
+	if (dialog_add_button(alert_dialog, "Cancel", true, cancel_button_context, true) != BPS_SUCCESS)
+	{
+		fprintf(stderr, "Failed to add button to alert dialog.");
+		dialog_destroy(alert_dialog);
+		alert_dialog = 0;
+	}
+
+	if (dialog_show(alert_dialog) != BPS_SUCCESS)
+	{
+		fprintf(stderr, "Failed to show alert dialog.");
+		dialog_destroy(alert_dialog);
+		alert_dialog = 0;
+	}
+
+	while(1)
+	{
+		bps_event_t* event = NULL;
+		bps_get_event(&event, -1);
+
+		if (event)
+		{
+			if (bps_event_get_domain(event) == dialog_get_domain())
+			{
+				const char* context = dialog_event_get_selected_context(event);
+				if (stricmp(context, ok_button_context) == 0)
+					retval = OK_BUTTON;
+				else if (stricmp(context, cancel_button_context) == 0)
+					retval = CANCEL_BUTTON;
+				break;
+			}
+		}
+	}
+	if (alert_dialog)
+		dialog_destroy(alert_dialog);
+
+	return retval;
 }
 
 int showRomInfo()
@@ -250,7 +325,7 @@ int AutoLoadRom(int &M, bool showInfo)
 	bps_event_t *event;
 	int domain = 0;
 	const char* label;
-	char romfilename[256];
+	//char romfilename[256];
 	dialog_create_popuplist(&dialog);
 	dialog_set_popuplist_items(dialog, list, count);
 	if (showInfo)
@@ -287,7 +362,8 @@ int AutoLoadRom(int &M, bool showInfo)
 						{
 							printf("%s\n", list[*response[0]]);
 							fflush(stdout);
-							strcpy(romfilename, list[*response[0]]);
+							//strcpy(romfilename, list[*response[0]]);
+							strcpy(TIFilename, list[*response[0]]);
 						}
 						else
 						{
@@ -312,7 +388,8 @@ int AutoLoadRom(int &M, bool showInfo)
 
 	free(list);
 
-	return WhichTI(romfilename, M);
+	//return WhichTI(romfilename, M);
+	return WhichTI(TIFilename, M);
 }
 
 void ToggleEffects()
@@ -454,13 +531,25 @@ void ProgramInfo()
 
 int doContextMenu(int &M)
 {
+	deviceinfo_details_t* data;
+	deviceinfo_get_details(&data);
+	const char* os = deviceinfo_details_get_device_os(data);
+
+	if (strcmp(os, "BlackBerry 10") == 0)
+	{
+		deviceinfo_free_details(&data);
+		return -1;
+	}
+	deviceinfo_free_details(&data);
 	dialog_instance_t context;
 
 	dialog_create_context_menu(&context);
-	dialog_set_context_menu_coordinates(context, 300, 1024);
+	//dialog_set_context_menu_coordinates(context, 300, 1024);
 	dialog_request_events(0);
 
-	dialog_context_menu_add_button(context, "Load ROM", true, "LOAD", true, DIALOG_OPEN_LINK_ICON);
+	bool files = UpdateRomList(0);
+
+	dialog_context_menu_add_button(context, "Load ROM", files, "LOAD", true, DIALOG_OPEN_LINK_ICON);
 #ifdef DEBUG
 	dialog_context_menu_add_button(context, "Toggle Effects", true, "EFFECTS", true, DIALOG_SELECT_ICON);
 #endif
@@ -507,7 +596,6 @@ int doContextMenu(int &M)
 	switch (selected)
 	{
 	case 0:
-		UpdateRomList();
 		return AutoLoadRom(M);
 	case 1:
 		ProgramInfo();
